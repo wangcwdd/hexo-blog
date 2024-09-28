305d4319028c44b5b07c6b5aac7cf0c83401e23e
wangchao <913297683@qq.com>
2024-09-29 01:19:06 +0800

    Site updated: 2024-09-29 01:19:06

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/28/hello-world/"];function toRandomPost(){
+var posts=["2024/09/28/hello-world/","2024/09/29/my-first-blog/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
