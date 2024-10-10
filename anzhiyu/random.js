131585bb9d3e3a7b474c6d231c538d3d9d927297
wangchao <913297683@qq.com>
2024-10-11 00:30:04 +0800

    Site updated: 2024-10-11 00:30:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/28/hello-world/","2024/09/29/my-first-blog/"];function toRandomPost(){
+var posts=["2024/10/10/第一篇博文/","2024/09/28/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
