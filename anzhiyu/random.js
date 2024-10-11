4056f7dc6d44dc9536be50b19140f6f20ac026e4
wangchao <913297683@qq.com>
2024-10-11 22:12:16 +0800

    Site updated: 2024-10-11 22:12:15

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/10/第一篇博文/","2024/09/28/hello-world/"];function toRandomPost(){
+var posts=["2024/10/11/SpringCloud学习-Gateway/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
